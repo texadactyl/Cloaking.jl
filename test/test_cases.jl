06c991c5e49b4a408ce5676c6756e889b37f75db
Richard Elkins <richard.elkins@gmail.com>
2021-07-26 14:53:28 -0500

    Fix initial bugs in test_cases.jl

--- test/test_cases.jl
+++ test/test_cases.jl
@@ -1,26 +1,26 @@
 using Printf
-include("cloak_file.jl")
-include("uncloak_file.jl")
+using Cloaking
 
 
 function do_case(id::Integer, cleartext::AbstractString, password::AbstractString; outdir="./test_data/", debugging=false)
     @printf("\n=== Text Case %03d ====\n", id)
-    
-    ix = findlast(isequal('.'),basename(cleartext))
+    filename = basename(cleartext)
+    ix = findlast(isequal('.'), filename)
     if ix == nothing
-        filestem = cleartext
+        filestem = filename
     else
-        filestem = cleartext[1 : ix - 1]
+        filestem = filename[1 : ix - 1]
+    end
     outdir = abspath(outdir)
-    encfile = joinpath(outdir, string(filestem, "enc"))
-    decfile = joinpath(outdir, string(filestem, "dec"))
+    encfile = joinpath(outdir, string(filestem, ".enc"))
+    decfile = joinpath(outdir, string(filestem, ".dec"))
     et, ncipherbytes = cloak_file(password, cleartext, encfile, debugging=debugging)
     et, nclearbytes = uncloak_file(password, encfile, decfile, debugging=debugging)
 
-    @printf("one_case: cloak   --> %s, E.T. = %d s, # of encrypted bytes = %d\n", basename(encfile), et, ncipherbytes)
-    @printf("one_case: uncloak --> %s, E.T. = %d s, # of decrypted bytes = %d\n", basename(decfile), et, nclearbytes)
+    @printf("one_case: cloak   --> %s, E.T. = %d s, # of encrypted bytes = %d\n", encfile, et, ncipherbytes)
+    @printf("one_case: uncloak --> %s, E.T. = %d s, # of decrypted bytes = %d\n", decfile, et, nclearbytes)
 
-    cmd = `cmp $CLEARTEXT_1 $CLEARTEXT_2`
+    cmd = `cmp $cleartext $decfile`
     if success(cmd)
         println("\nSuccessful comparison")
     else
@@ -38,6 +38,6 @@ end
 do_case(1, string(datadir, "mary_1120.txt"), password)
 do_case(2, string(datadir, "mary_1121.txt"), password)
 do_case(3, string(datadir, "123.txt"), password)
-#do_case(4, "/boot/initrd.img", password)
+do_case(4, "/boot/initrd.img", password)
 
 
